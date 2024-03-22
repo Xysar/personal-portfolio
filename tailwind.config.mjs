c86e56f8b5d91e3127052c2de125d0bba3dd5dba
Cesar Camacho <cesarcamacho760@gmail.com>
2024-03-22 00:34:49 -0700

    make canvas responsive

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
+      colors: {
+        navy: "#212539",
+      },
       fontFamily: {
         league: ["league-spartan, sans-serif"],
       },
